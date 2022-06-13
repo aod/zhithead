401b2744f03dc1f1c12788487753d7516b369b42
Oktay <oktay@yatko.dev>
2022-06-13 13:23:36 +0200

    Add TailwindCSS

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
 export default function App() {
   return (
-    <main>
-      <h1>Zhithead</h1>
+    <main className="bg-zinc-800 h-screen grid place-items-center">
+      <h1 className="text-zinc-300 text-8xl uppercase font-bold tracking-wide">
+        Zhithead
+      </h1>
     </main>
   );
 }
