138ab290b603db3eb4a76e79f76b86466ab1a0a9
Oktay <oktay@yatko.dev>
2022-06-16 13:49:38 +0200

    Attempt an animation of playing a card

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import Hand from "./Hand";
 
 export default function App() {
   return (
-    <main className="grid h-screen place-items-center bg-zinc-800">
+    <main className="flex h-screen items-end justify-center bg-zinc-800">
       <Hand />
     </main>
   );
--- src/Hand.tsx
+++ src/Hand.tsx
@@ -1,68 +1,91 @@
 import { motion } from "framer-motion";
 import { useState } from "react";
 import Card from "./Card";
-import { createCard, Rank, Suite } from "./lib";
+import { createCard, Rank, Suite, Card as LibCard } from "./lib";
 
 export default function Hand() {
-  const [hoveredCardIdx, setHoveredCardIdx] = useState<number>(-2);
+  const [hoveredCardIdx, setHoveredCardIdx] = useState<number | null>(null);
+  const [played, setPlayed] = useState<LibCard[]>([]);
 
-  const deck = [
+  const hand = [
     createCard(Suite.Clubs, Rank.Num6),
     createCard(Suite.Clubs, Rank.Num9),
+    createCard(Suite.Diamonds, Rank.Num2),
+    createCard(Suite.Diamonds, Rank.Ace),
     createCard(Suite.Hearts, Rank.Queen),
     createCard(Suite.Hearts, Rank.King),
-    createCard(Suite.Hearts, Rank.King),
-    createCard(Suite.Hearts, Rank.King),
-    createCard(Suite.Hearts, Rank.King),
-    createCard(Suite.Hearts, Rank.King),
-    createCard(Suite.Hearts, Rank.King),
+    createCard(Suite.Hearts, Rank.Jack),
+    createCard(Suite.Hearts, Rank.Ace),
+    createCard(Suite.Hearts, Rank.Num4),
+    createCard(Suite.Hearts, Rank.Num2),
   ];
 
-  const width = 200;
+  const width = 175;
   const overlap = width / 2;
 
+  const createVariant = (isRight: boolean) => ({
+    rotate: `${3 * (isRight ? 1 : -1)}deg`,
+    translateY: "-10%",
+    scale: 1.1,
+    translateX: `${12.5 * (isRight ? 1 : -1)}%`,
+  });
   const variants = {
-    left: {
-      rotate: "-3deg",
-      translateY: "-10%",
-      scale: 1.1,
-      translateX: `-12.5%`,
-    },
-    right: {
-      rotate: "3deg",
-      translateY: "-10%",
-      scale: 1.1,
-      translateX: `12.5%`,
+    left: createVariant(false),
+    right: createVariant(true),
+    played: {
+      left: "50%",
+      top: "50%",
+      translateY: "-50%",
     },
   };
 
-  const siblings: Record<number, string> = {
-    [-1]: "right",
-    [1]: "left",
+  const chooseVariant = (index: number) => {
+    if (played.includes(hand[index])) {
+      return "played";
+    }
+    if (hoveredCardIdx === null) {
+      return;
+    }
+    for (let i = hoveredCardIdx + 1; i <= index; i++) {
+      if (!played.includes(hand[i]) && i !== index) break;
+      if (i === index) return "right";
+    }
+    for (let i = hoveredCardIdx - 1; i >= 0; i--) {
+      if (!played.includes(hand[i]) && i !== index) break;
+      if (i === index) return "left";
+    }
   };
 
   return (
     <div
-      className="flex w-full flex-nowrap items-end justify-center overflow-hidden py-[200px]"
+      className="flex w-full flex-nowrap items-end justify-center pb-8"
       style={{ paddingLeft: overlap }}
     >
-      {deck.map((card, i) => (
+      {hand.map((card, i) => (
         <motion.div
-          drag
-          dragElastic={0.2}
-          animate={siblings[hoveredCardIdx - i]}
+          animate={chooseVariant(i)}
           variants={variants}
-          dragConstraints={{ left: 0, right: 0, bottom: 0, top: -50 }}
-          whileHover={{ scale: 1.2, translateY: "-20%" }}
+          whileHover={
+            played.includes(card) ? {} : { scale: 1.2, translateY: "-20%" }
+          }
           whileTap={{ scale: 1 }}
-          onHoverStart={() => setHoveredCardIdx(i)}
-          onHoverEnd={() => setHoveredCardIdx(-2)}
+          onHoverStart={() => !played.includes(card) && setHoveredCardIdx(i)}
+          onHoverEnd={() => setHoveredCardIdx(null)}
           className="origin-center"
           style={{
             marginLeft: -overlap,
             width,
+            position: played.includes(card) ? "absolute" : "initial",
+            zIndex: Math.max(
+              played.findIndex((val) => val === card),
+              0
+            ),
           }}
-          key={i}
+          key={card}
+          onClick={() => {
+            setPlayed([...played, card]);
+            setHoveredCardIdx(null);
+          }}
         >
           <Card card={card} />
         </motion.div>
