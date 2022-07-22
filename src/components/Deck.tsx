f14ac445ef094def2096ca1b6f33b08d22dc5df8
Oktay <oktay@yatko.dev>
2022-07-22 14:58:50 +0200

    Display deck in playing phase

--- src/components/Deck.tsx
+++ src/components/Deck.tsx
@@ -1,9 +1,30 @@
+import { useSelector } from "@xstate/react";
+import clsx from "clsx";
+import { useContext } from "react";
 import Card from "./Card";
+import { GlobalStateContext } from "./GlobalStateProvider";
 
 export default function Deck() {
+  const globalServices = useContext(GlobalStateContext);
+  const hasDeck = useSelector(
+    globalServices.zhitheadService,
+    (state) => state.context.deck.length
+  );
+
   return (
-    <div>
-      <Card flipped />
+    <div className="relative box-content h-card-height w-card-width rounded-xl border-2 border-dashed border-zinc-600 p-0.5">
+      {hasDeck && <Card flipped />}
+      <div className="absolute left-0 top-0 flex h-full w-full items-center justify-center">
+        <span
+          className={clsx(
+            "select-none rounded-lg  text-4xl font-bold tracking-wider",
+            hasDeck && "bg-zinc-800/40 p-2 text-zinc-200",
+            !hasDeck && "text-zinc-600"
+          )}
+        >
+          DECK
+        </span>
+      </div>
     </div>
   );
 }
--- src/components/phases/Playing.tsx
+++ src/components/phases/Playing.tsx
@@ -1,11 +1,15 @@
 import Pile from "../Pile";
 import Switcher from "../Switcher";
 import ShownHand from "../ShownHand";
+import Deck from "../Deck";
 
 export default function Playing() {
   return (
-    <div className="flex h-full flex-col items-center justify-end gap-24 pb-10">
-      <Pile />
+    <div className="flex h-full flex-col items-center justify-end gap-14 pb-10">
+      <div className="flex gap-16">
+        <Deck />
+        <Pile />
+      </div>
       <div className="space-y-14">
         <Switcher />
         <ShownHand />
