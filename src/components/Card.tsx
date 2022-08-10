b902af3979636aeef63278fe6c49897ea190c975
Oktay <oktay@yatko.dev>
2022-08-10 15:43:27 +0200

    Refactor svg path generation in card component
    Don't use enum names/values for path generation. Instead decouple it
    with a LUT for Suite and Rank and allow them to freely change
    name/order/value.

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,26 +1,17 @@
 import clsx from "clsx";
 import { motion } from "framer-motion";
-import { Card as LibCard, getRank, getSuite, Rank, Suite } from "../lib";
-
-function createCardSVGPath(card: LibCard) {
-  const suitePathComponent = Suite[getSuite(card)].toUpperCase().slice(0, -1);
-  const r = getRank(card);
-  const rankPathComponent = `${r + 1}${
-    r >= Rank.Jack ? `-${Rank[r].toUpperCase()}` : ""
-  }`;
-  return `/cards/${suitePathComponent}-${rankPathComponent}.svg`;
-}
+import { Card as TCard, getRank, getSuite, Rank, Suite } from "../lib";
 
 export interface CardProps {
-  card?: LibCard;
+  card?: TCard;
   flipped?: boolean;
-  onClick?: (card: LibCard | undefined) => void;
+  onClick?: (card: TCard | undefined) => void;
 }
 
 export default function Card(props: CardProps) {
   const src =
     props.flipped || props.card === undefined
-      ? "/cards/BACK.svg"
+      ? CARD_BACK_SVG_PATH
       : createCardSVGPath(props.card);
 
   return (
@@ -35,3 +26,39 @@ export default function Card(props: CardProps) {
     />
   );
 }
+
+const CARDS_PREFIX_PATH = "/cards/";
+const CARD_BACK_SVG_PATH = `${CARDS_PREFIX_PATH}BACK.svg`;
+
+function createCardSVGPath(card: TCard) {
+  return (
+    CARDS_PREFIX_PATH +
+    SuitePathCompLUT[getSuite(card)] +
+    "-" +
+    RankPathCompLUT[getRank(card)] +
+    ".svg"
+  );
+}
+
+const SuitePathCompLUT: Record<Suite, string> = {
+  [Suite.Clubs]: "CLUB",
+  [Suite.Diamonds]: "DIAMOND",
+  [Suite.Hearts]: "HEART",
+  [Suite.Spades]: "SPADE",
+};
+
+const RankPathCompLUT: Record<Rank, string> = {
+  [Rank.Ace]: "1",
+  [Rank.Num2]: "2",
+  [Rank.Num3]: "3",
+  [Rank.Num4]: "4",
+  [Rank.Num5]: "5",
+  [Rank.Num6]: "6",
+  [Rank.Num7]: "7",
+  [Rank.Num8]: "8",
+  [Rank.Num9]: "9",
+  [Rank.Num10]: "10",
+  [Rank.Jack]: "11-JACK",
+  [Rank.Queen]: "12-QUEEN",
+  [Rank.King]: "13-KING",
+};
