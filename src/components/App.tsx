65081881f5721a3f8b8cb19797d50c2388e8ff41
Oktay <oktay@yatko.dev>
2022-08-17 01:45:56 +0200

    Fix cards jumping in hand on transition
    Revert screens refactoring and put state matching inside main App
    component. Keep It Simple.

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,9 +1,12 @@
 import { useActor } from "@xstate/react";
 import { useContext } from "react";
 import { States } from "../state";
+import Deck from "./Deck";
 import { GlobalStateContext } from "./GlobalStateProvider";
-import ChoosingFaceUpCards from "./screens/ChoosingFaceUpCards";
-import Playing from "./screens/Playing";
+import OffHand from "./OffHand";
+import Pile from "./Pile";
+import ShownHand from "./ShownHand";
+import Switcher from "./Switcher";
 
 export default function App() {
   const globalServices = useContext(GlobalStateContext);
@@ -11,8 +14,25 @@ export default function App() {
 
   return (
     <main className="h-screen overflow-hidden bg-zinc-800">
-      {state.matches(States.choosingFaceUpCards) && <ChoosingFaceUpCards />}
-      {state.matches(States.playing) && <Playing />}
+      <div className="h-full">
+        <div className="flex h-full items-center justify-center gap-12">
+          {state.matches(States.choosingFaceUpCards) && <OffHand />}
+          {state.matches(States.playing) && (
+            <>
+              <Deck />
+              <Pile />
+            </>
+          )}
+        </div>
+        <div className="relative bottom-[calc(var(--card-height)*0.85)]">
+          {state.matches(States.playing) && (
+            <div className="absolute -top-20 mx-auto w-full">
+              <Switcher />
+            </div>
+          )}
+          <ShownHand />
+        </div>
+      </div>
     </main>
   );
 }
