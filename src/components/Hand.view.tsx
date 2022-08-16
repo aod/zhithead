573ec52851d94e078ef6cebc02e3d189c5fc7e8b
Oktay <oktay@yatko.dev>
2022-08-17 01:53:52 +0200

    Resize hand on window resize

--- src/components/Hand.view.tsx
+++ src/components/Hand.view.tsx
@@ -1,5 +1,5 @@
 import { motion, Variants } from "framer-motion";
-import { useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import Card from "./Card";
 import { Cards } from "../lib";
 
@@ -11,9 +11,18 @@ interface HandProps {
 }
 
 export default function HandView(props: HandProps) {
-  function onCardClick(index: number) {
-    props.onCardClick(index);
-  }
+  const ref = useRef<HTMLDivElement | null>(null);
+  const [handWidth, setHandWidth] = useState(0);
+  const virtualFanWidth = Math.min(handWidth, props.hand.length * 100);
+  const virtualFanHeight = virtualFanWidth * 0.75;
+
+  const hasRef = ref.current !== undefined;
+  useEffect(() => {
+    const onResize = () => setHandWidth(ref.current!.clientWidth);
+    onResize();
+    window.addEventListener("resize", onResize);
+    return () => window.removeEventListener("resize", onResize);
+  }, [hasRef]);
 
   function angle(i: number) {
     const factor = props.hand.length / 4;
@@ -21,13 +30,6 @@ export default function HandView(props: HandProps) {
     if (props.hand.length % 2 === 0) x += 0.025;
     return x * (Math.PI / factor);
   }
-
-  const ref = useRef<HTMLDivElement | null>(null);
-  const virtualFanWidth = Math.min(
-    ref.current?.clientWidth ?? Infinity,
-    props.hand.length * 100
-  );
-  const virtualFanHeight = virtualFanWidth * 0.75;
 
   const variants: Variants = {
     show: (i: number) => ({
@@ -57,7 +59,7 @@ export default function HandView(props: HandProps) {
         >
           <Card
             card={card}
-            onClick={() => onCardClick(i)}
+            onClick={() => props.onCardClick(i)}
             flipped={props.hideCards}
           />
         </motion.div>
