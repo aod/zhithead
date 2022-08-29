01839be3739d36f87f9f63f58fd5335cf3befde8
Oktay <oktay@yatko.dev>
2022-08-29 12:53:49 +0200

    Add playing rules: check card value & burning/taking pile

--- src/state/machines/zhithead.machine.ts
+++ src/state/machines/zhithead.machine.ts
@@ -1,7 +1,15 @@
 import { assign } from "@xstate/immer";
 import { ContextFrom, send } from "xstate";
 import { createModel } from "xstate/lib/model";
-import { Card, createDeck, dealCardsFor, Player as TPlayer } from "../../lib";
+import {
+  canPlay,
+  Card,
+  createDeck,
+  dealCardsFor,
+  getRank,
+  Player as TPlayer,
+  Rank,
+} from "../../lib";
 import humanMachine from "./human.machine";
 import { PlayerEvents, barePlayerEvent } from "../shared/player-events";
 import { createBotService } from "../services/bot.service";
@@ -47,6 +55,7 @@ export const zhitheadModel = createModel(createInitialContext(), {
       player,
     }),
     TAKE_CARD: () => ({}),
+    TAKE_PILE: () => ({}),
     ...barePlayerEvent("CARD_CHOSEN"),
   },
 });
@@ -107,19 +116,43 @@ export const zhitheadMachine = zhitheadModel.createMachine(
                     {
                       target: "#loop.waitForMove",
                       actions: ["takePile", "switchTurns"],
+                      // Bot returns undefined when no cards could be played.
+                      // event.card from human should never be null.
                       cond: (_, event) => event.card === undefined,
                     },
                     {
-                      target: "#loop.delayedDraw",
+                      target: "#loop.beforeNewMove",
                       actions: "play",
+                      cond: (context, event) =>
+                        canPlay(event.card!, context.pile),
+                    },
+                    {
+                      target: "#loop.waitForMove", // Ask again
                     },
                   ],
+                  TAKE_PILE: {
+                    target: "#loop.waitForMove",
+                    actions: ["takePile", "switchTurns"],
+                    cond: (context) =>
+                      context.currentTurn === "human" &&
+                      !canCurrentPlayerPlayCard(context),
+                  },
                 },
               },
-              delayedDraw: {
+              beforeNewMove: {
                 after: {
-                  225: {
-                    actions: ["takeCard", "switchTurns"],
+                  600: {
+                    actions: "burnPile",
+                    cond: {
+                      type: "topOfPileRankEquals",
+                      rank: Rank.Num10,
+                    },
+                  },
+                  625: {
+                    actions: "takeCard",
+                  },
+                  1000: {
+                    actions: ["switchTurns"],
                     target: "#loop.waitForMove",
                   },
                 },
@@ -173,17 +206,43 @@ export const zhitheadMachine = zhitheadModel.createMachine(
         const card = context.deck.pop();
         if (card !== undefined) context[context.currentTurn].hand.push(card);
       }),
+      burnPile: assign((context) => {
+        context.pile = [];
+      }),
     },
-    // guards: {
-    //   invalidCard: (_, event) => {
-    //     return event.type === "CARD_CHOSEN" && event.card === undefined;
-    //   },
-    // },
+    guards: {
+      topOfPileRankEquals: (context, _, { cond }) => {
+        const top = context.pile.at(-1);
+        // FIXME
+        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+        // @ts-ignore
+        const rank: Rank = cond.rank;
+        return top !== undefined && getRank(top) === rank;
+      },
+    },
   }
 );
 
-function hasChoosenAllFaceUpCards(context: ContextFrom<typeof zhitheadModel>) {
+function hasChoosenAllFaceUpCards(
+  context: ContextFrom<typeof zhitheadModel>
+): boolean {
   return context.human.offHand.faceUp.length === 3;
+}
+
+function canCurrentPlayerPlayCard(
+  context: ContextFrom<typeof zhitheadModel>
+): boolean {
+  const hands = [
+    context[context.currentTurn].hand,
+    context[context.currentTurn].offHand.faceUp,
+    context[context.currentTurn].offHand.faceDown,
+  ];
+  const handIndex = hands.findIndex((hand) => hand.length);
+  if (handIndex === -1) return false;
+
+  const hand = hands[handIndex];
+  if (handIndex < 2) return hand.some((card) => canPlay(card, context.pile));
+  return true;
 }
 
 function shuffle<T>(array: T[]): T[] {
