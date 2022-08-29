51260e53af31ee2e560698395d408624cd700cb4
Oktay <oktay@yatko.dev>
2022-08-29 12:52:39 +0200

    Make card face with rank 8 transparent to see card underneath

--- src/components/Pile.tsx
+++ src/components/Pile.tsx
@@ -1,6 +1,7 @@
 import { useSelector } from "@xstate/react";
 import { AnimatePresence, motion } from "framer-motion";
 import { useContext } from "react";
+import { getRank, Rank } from "../lib";
 import Card from "./Card";
 import Count from "./Count";
 import { GlobalStateContext } from "./GlobalStateProvider";
@@ -8,19 +9,52 @@ import { GlobalStateContext } from "./GlobalStateProvider";
 export default function Pile() {
   const { zhitheadService } = useContext(GlobalStateContext);
   const pile = useSelector(zhitheadService, (state) => state.context.pile);
+  const { send } = zhitheadService;
+  const pileWithout8s = pile.filter((card) => getRank(card) !== Rank.Num8);
 
   return (
     <div className="relative box-content flex h-card-height w-card-width justify-center rounded-xl border-2 border-dashed border-zinc-600 p-0.5">
-      {pile.length >= 2 && (
+      {pileWithout8s.length >= 2 && (
         <div className="absolute">
-          <Card card={pile.at(-2)} />
+          <Card card={pileWithout8s.at(-2)} />
         </div>
       )}
-      {pile.length >= 1 && (
-        <div className="absolute">
-          <Card key={pile.length} card={pile.at(-1)} z={1} />
+      {pileWithout8s.length >= 1 && (
+        <div className="absolute" style={{ zIndex: 1 }}>
+          <Card
+            key={pileWithout8s.length}
+            card={pileWithout8s.at(-1)}
+            onClick={() => send({ type: "TAKE_PILE" })}
+          />
         </div>
       )}
+      <AnimatePresence>
+        {pile.length >= 1 && getRank(pile.at(-1)!) === Rank.Num8 && (
+          <motion.div
+            key={pile.at(-1)!}
+            className="absolute h-full w-full"
+            initial={{ left: 0, top: 0, filter: "opacity(1)", opacity: 1 }}
+            animate={{
+              x: 25,
+              y: 20,
+              transition: {
+                delay: 0.6,
+                type: "tween",
+              },
+              filter: "opacity(0.9)",
+            }}
+            exit={{
+              filter: "opacity(1)",
+              x: 0,
+              y: 0,
+              zIndex: 0,
+            }}
+            style={{ zIndex: 2 }}
+          >
+            <Card card={pile.at(-1)} />
+          </motion.div>
+        )}
+      </AnimatePresence>
       <Count count={pile.length} position="top-left" z={2} />
       <AnimatePresence>{!pile.length && <Text />}</AnimatePresence>
     </div>
